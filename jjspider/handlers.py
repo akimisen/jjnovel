97367ab3cc9a3e7cbf7729919595184a2a0df312
Tracy <akimisen@gmail.com>
2023-02-22 07:36:02 +0800

    Update csv and handlers

--- jjspider/handlers.py
+++ jjspider/handlers.py
@@ -2,13 +2,23 @@ import pandas as pd
 import ast
 
 def extract_novels():
-  _data = pd.read_csv('data/authors.csv',index_col='id', encoding='gbk')
+  _data = pd.read_csv('data/authors.csv',index_col='id', encoding='utf-8')
   data=_data.reindex(columns=['name','novels','flw_count','score'])
   novels=[]
   for index,row in data.iterrows():
     for n in ast.literal_eval(row['novels']):
       novels.append((index,row['name'],int(n)))
   df2=pd.DataFrame(novels,columns=['aid','author','nid'])
-  df2.to_csv('data/novels_of_author.csv',index=False,encoding='gbk')
+  df2.to_csv('data/novels_of_author.csv',index=False,encoding='utf-8')
 
-extract_novels()
+# def query_novels(raw_csv_path):
+raw_csv_path='data/newdaylist_author_novels-[raw][20230116-20230216].csv'
+_data=pd.read_csv(raw_csv_path,index_col='nid', encoding='utf-8', thousands=',')
+data=_data.reindex(columns=['title','author','like_count','avg_click','word_count','chapter_count','is_signed','created','updated','yc','xx','era','style','score','copystatus'])
+grouped=data.groupby('author')
+novel_count=grouped['title'].count()
+avg_word_count=grouped['word_count'].mean()
+# copyrights=(grouped['copystatus']=='1').sum()
+print(novel_count)
+print(avg_word_count)
+# print(copyrights)
--- jjspider/utils.py
+++ jjspider/utils.py
@@ -4,40 +4,22 @@ def chinese_to_number(chinese_num):
   """
   Convert Chinese numerical characters to their corresponding numerical values.
   """
-  chinese_to_arabic = {
-    '〇': 0,
-    '一': 1,
-    '二': 2,
-    '三': 3,
-    '四': 4,
-    '五': 5,
-    '六': 6,
-    '七': 7,
-    '八': 8,
-    '九': 9,
-  }
-  
-  pattern = re.compile(r'([〇一二三四五六七八九]+)([十百千万亿]?)')
-  result = pattern.findall(chinese_num)
+  pattern = re.compile(r'([0-9]+)([十百千万亿]?)')
+  result = pattern.search(chinese_num)
   number = 0
-  
-  for chinese_digit, chinese_unit in result:
-      digit = chinese_to_arabic[chinese_digit]
-      unit = 1
-      
-      if chinese_unit == '十':
-          unit = 10
-      elif chinese_unit == '百':
-          unit = 100
-      elif chinese_unit == '千':
-          unit = 1000
-      elif chinese_unit == '万':
-          unit = 10000
-      elif chinese_unit == '亿':
-          unit = 100000000
-      
-      number += digit * unit
-  
+  digit = result[0]
+  unit = 1
+  if result[-1] == '十':
+    unit = 10
+  elif result[-1] == '百':
+    unit = 100
+  elif result[-1] == '千':
+    unit = 1000
+  elif result[-1] == '万':
+    unit = 10000
+  elif result[-1] == '亿':
+    unit = 100000000
+  number += digit * unit
   return number
 
 def split_yc(novelclass):
